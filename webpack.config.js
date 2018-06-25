9e7ffef7a7519bb624ce1f96613b4ae576ab26b2
Feddle <sepptee@outlook.com>
2018-06-25 17:23:09 +0300

    added scss loading to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,10 @@
+require("dotenv").config();
 const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
     mode: process.env.ENVIRONMENT,
-    entry: ["babel-polyfill", "./clientjs/mybooks.js"],    
+    entry: ["babel-polyfill", "./clientjs/mybooks.js", "./styles/style.scss"],    
     output: {
         path: path.resolve(__dirname, "public"),
         filename: "bundle.js"
@@ -18,7 +20,23 @@ module.exports = {
                         presets: ["babel-preset-env"]
                     }
                 }
+            },
+            {
+                test: /\.scss$/,
+                use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                    "file-loader"
+                ]
             }
         ]
-    }
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "[name].css",
+            chunkFilename: "[id].css"
+        })
+    ]
 };
